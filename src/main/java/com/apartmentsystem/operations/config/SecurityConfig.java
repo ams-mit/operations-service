ef0ef50086ff02964045b832db03034a509c9c17
Poojani Samarasekara <poojanisamarasekara16@gmail.com>
2026-09-25 20:53:40 +0530

    AMS-G4-5 add method security configuration

--- src/main/java/com/apartmentsystem/operations/config/SecurityConfig.java
+++ src/main/java/com/apartmentsystem/operations/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.apartmentsystem.operations.config;
 import com.apartmentsystem.operations.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
